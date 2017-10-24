b7fa9679d47d7a4ba83448fd18f047f7344cb1eb
erksch <erik.ziegler@student.hpi.uni-potsdam.de>
2017-10-24 16:35:44 +0200

    Added author model

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 describe "Author", :type => :model do
   let(:author) {
-     Author.new(true, first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
+     Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
   }
 
   it 'should have correct first name, last name and homepage' do
-
     expect(author.first_name).to eq('Alan')
     expect(author.last_name).to eq('Turing')
     expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
