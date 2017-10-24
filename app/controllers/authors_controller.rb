048192fb1cba6f7da2e819950821759b12da91ba
erksch <erik.ziegler@student.hpi.uni-potsdam.de>
2017-10-24 17:13:47 +0200

    Added create method and test for new author form

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,19 @@
 
 class AuthorsController < ApplicationController
-  # GET /authors/new
-  def new
+  # POST /authors/new
+  def create
+    @author = Author.new(author_params)
 
+    @author.save
+    render plain: ''
+  end
+
+  # GET /authors/new
+  def new; end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:first_name, :last_name, :homepage)
   end
 end
