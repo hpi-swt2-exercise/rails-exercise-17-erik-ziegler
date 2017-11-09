b85b25212235819a67409cb336b243b2338986d2
erksch <erik.ziegler@student.hpi.uni-potsdam.de>
2017-11-09 17:12:39 +0100

    Failing test for #15.

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -6,4 +6,13 @@ describe "Edit author page", type: :feature do
     @alan = FactoryGirl.create :author
     visit edit_author_path @alan
   end
+
+  it "should be able to change first name without error" do
+    @alan = FactoryGirl.create :author
+    visit edit_author_path @alan
+
+    fill_in "author_first_name", :with => 'Alan Mathison'
+
+    find('input[type="submit"]').click
+  end
 end
