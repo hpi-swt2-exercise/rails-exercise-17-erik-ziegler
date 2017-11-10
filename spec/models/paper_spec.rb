4ec2b68f37505c9767b9c2f3eb14ff94f350d39c
erksch <erik.ziegler@student.hpi.uni-potsdam.de>
2017-11-10 11:58:21 +0100

    Failing test for #22.

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe "Paper", :type => :model do
   let(:paper_without_title) {Paper.new(title: nil, venue: 'mind 49: 433-460', year: 1950)}
   let(:paper_without_venue) {Paper.new(title: 'computing machinery and intelligence', venue: nil, year: 1950)}
+  let(:paper_without_year) {Paper.new(title: 'computing machinery and intelligence', venue: 'mind 49: 433-460', year: nil)}
 
   it "should be invalid without title" do
     expect(paper_without_title).to_not be_valid
@@ -10,5 +11,9 @@ describe "Paper", :type => :model do
 
   it "should be invalid without venue" do
     expect(paper_without_venue).to_not be_valid
+  end
+
+  it "should be invalid without year" do
+    expect(paper_without_year).to_not be_valid
   end
 end
