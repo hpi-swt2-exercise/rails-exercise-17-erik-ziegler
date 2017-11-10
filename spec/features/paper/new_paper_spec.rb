a251aac161f372170080e2da745baf2b1118c204
erksch <erik.ziegler@student.hpi.uni-potsdam.de>
2017-11-10 11:27:13 +0100

    Failing test for #19.

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -13,4 +13,19 @@ describe "New paper page", type: :feature do
     expect(page).to have_field('Venue')
     expect(page).to have_field('Year')
   end
+
+  it "should create correct instance" do
+    visit new_paper_path
+
+    fill_in "paper_title", :with => 'COMPUTING MACHINERY AND INTELLIGENCE'
+    fill_in "paper_venue", :with => 'Mind 49: 433-460'
+    fill_in "paper_year", :with => 1950
+
+    find('input[type="submit"]').click
+
+    paper = Paper.last
+    expect(paper.title).to eq('COMPUTING MACHINERY AND INTELLIGENCE')
+    expect(paper.venue).to eq('Mind 49: 433-460')
+    expect(paper.year).to eq(1950)
+  end
 end
