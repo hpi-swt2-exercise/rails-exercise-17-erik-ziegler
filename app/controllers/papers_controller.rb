61359da9bfc57e94a74e3ac29bcf9773c6b84ba7
erksch <erik.ziegler@student.hpi.uni-potsdam.de>
2017-11-10 11:37:05 +0100

    Create paper on new paper page. Closes #19.

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -3,4 +3,18 @@ class PapersController < ApplicationController
   # GET /papers/new
   def new
   end
+
+  # POST /papers/new
+  def create
+    @paper = Paper.new(paper_params)
+
+    @paper.save
+    redirect_to new_author_path
+  end
+
+  private
+
+  def paper_params
+    params.require(:paper).permit(:title, :venue, :year)
+  end
 end
