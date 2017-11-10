a2703109f72be39ff73764bc9b2bb703635d9b6a
erksch <erik.ziegler@student.hpi.uni-potsdam.de>
2017-11-10 12:06:42 +0100

    Failing test for #23.

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -4,6 +4,7 @@ describe "Paper", :type => :model do
   let(:paper_without_title) {Paper.new(title: nil, venue: 'mind 49: 433-460', year: 1950)}
   let(:paper_without_venue) {Paper.new(title: 'computing machinery and intelligence', venue: nil, year: 1950)}
   let(:paper_without_year) {Paper.new(title: 'computing machinery and intelligence', venue: 'mind 49: 433-460', year: nil)}
+  let(:paper_with_invalid_year) {Paper.new(title: 'computing machinery and intelligence', venue: 'mind 49: 433-460', year: 0)}
 
   it "should be invalid without title" do
     expect(paper_without_title).to_not be_valid
@@ -14,6 +15,10 @@ describe "Paper", :type => :model do
   end
 
   it "should be invalid without year" do
+    expect(paper_without_year).to_not be_valid
+  end
+
+  it "should be invalid with not-integer year" do
     expect(paper_without_year).to_not be_valid
   end
 end
