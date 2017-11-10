692fe7a4bdb92c10e8f9873be822656888caf3e1
erksch <erik.ziegler@student.hpi.uni-potsdam.de>
2017-11-10 09:44:57 +0100

    Failing test for #16.

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -16,7 +16,15 @@ describe "Index author page", type: :feature do
 
   it "should have add_author_link" do
     visit authors_path
-    expect(page).to have_css("a", "Add author")
+    expect(page).to have_css("a", text: "Add author")
+  end
+
+  it "should have edit author link" do
+    FactoryGirl.create :author
+    
+    visit authors_path
+
+    expect(page).to have_css("a", text: "Edit")
   end
 
   it "should have show link" do
@@ -24,6 +32,6 @@ describe "Index author page", type: :feature do
 
     visit authors_path
 
-    expect(page).to have_css("a", "Show")
+    expect(page).to have_css("a", text: "Show")
   end
 end
