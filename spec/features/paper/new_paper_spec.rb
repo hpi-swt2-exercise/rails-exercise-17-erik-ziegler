b38c440d2426009fba64e54c640bc106c7831a37
erksch <erik.ziegler@student.hpi.uni-potsdam.de>
2017-11-10 13:46:09 +0100

    Failing test for #32.

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -28,4 +28,15 @@ describe "New paper page", type: :feature do
     expect(paper.venue).to eq('Mind 49: 433-460')
     expect(paper.year).to eq(1950)
   end
+
+  it "should show error when submitted and title is blank" do
+    visit new_paper_path
+
+    fill_in "paper_title", :with => 'COMPUTING MACHINERY AND INTELLIGENCE'
+    fill_in "paper_year", :with => 1950
+
+    find('input[type="submit"]').click
+
+    expect(page).to have_content('Title can\'t be blank')
+  end
 end
